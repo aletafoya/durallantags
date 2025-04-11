2b6768efe155f6952a468b948b49d668f6f39a96
Andres <dlegadoandres0502@gmail.com>
2025-04-10 18:57:46 -0600

    Created components

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Navbar from "./Navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
