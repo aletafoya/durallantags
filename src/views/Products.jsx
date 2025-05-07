7d5b185d69f76e0475a4e262b44f3a7324c82353
aletafoya <ale.tufk@gmail.com>
2025-05-06 21:31:49 -0600

    ProductsPage

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase.js"; 
+import ProductCard from "../components/ProductCard";
+
 function Products() {
-  return <h1>Hola</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const querySnapshot = await getDocs(collection(db, "products_storage"));
+      const productList = querySnapshot.docs.map(doc => ({
+        id: doc["NO ART"],
+        ...doc.data(),
+      }));
+      setProducts(productList);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">Productos</h1>
+      <div>
+        {products.map(product => (
+          <ProductCard
+            name={product["Descripción"]}
+            description={product["Precio"]}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Products;
